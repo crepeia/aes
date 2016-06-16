0dd674215f66128316153fa3fb59a1f10fac926e
Thiago Rizuti <t.rizuti@gmail.com>
2016-06-16 09:42:59 -0300

    email sending task times

--- aes/src/java/aes/utility/Scheduler.java
+++ aes/src/java/aes/utility/Scheduler.java
@@ -3,6 +3,7 @@ package aes.utility;
 import aes.controller.ContactController;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.ejb.TransactionManagement;
@@ -27,7 +28,7 @@ public class Scheduler {
 
     }
     
-    //@Schedule(second = "0", minute = "0", hour = "0", dayOfWeek = "*")
+    @Schedule(second = "0", minute = "0", hour = "10", dayOfWeek = "*")
     public void sendEmails(){
         contactController.clearScheduledKeepingResultEmails();
         contactController.sendScheduledEmails();
