7669d9dbf19bb74c64270a8cf2188eb618c7e591
Leonardo Martins <leomartinsjf@gmail.com>
2016-10-13 11:36:49 -0300

    email sending fixes

--- aes/src/java/aes/utility/Scheduler.java
+++ aes/src/java/aes/utility/Scheduler.java
@@ -23,9 +23,22 @@ public class Scheduler {
     }
    
     
-    @Schedule(second = "0", minute = "0", hour = "9", dayOfWeek = "*")
+    @Schedule(second = "0", minute = "0", hour = "7", dayOfWeek = "*")
     public void sendEmails(){
         Logger.getLogger(Scheduler.class.getName()).log(Level.INFO, "AES - Morning Task Running");
+        contactController.sendScheduledEmails();
+    }
+    
+    
+    @Schedule(second = "0", minute = "0", hour = "15", dayOfWeek = "*")
+    public void afternoonTask() {
+        Logger.getLogger(Scheduler.class.getName()).log(Level.INFO, "AES - Afternoon task running");
+        contactController.sendScheduledEmails();
+    }
+    
+    @Schedule(second = "0", minute = "0", hour = "19", dayOfWeek = "*")
+    public void eveningTask() {
+        Logger.getLogger(Scheduler.class.getName()).log(Level.INFO, "AES - Evening task running");
         contactController.sendScheduledEmails();
     }
 
