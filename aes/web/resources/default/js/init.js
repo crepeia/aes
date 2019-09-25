aea24cdf27d11ef20b68a3712d093a58ff78723c
bmarcosps <brunomarcos@ice.ufjf.br>
2019-09-24 21:53:59 -0300

    Add bootstrap framework

--- aes/web/resources/default/js/init.js
+++ aes/web/resources/default/js/init.js
@@ -4,4 +4,9 @@
       $(".sidenav").sidenav();
     });
   }); // end of document ready
+  
+  $("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+    });
 })(jQuery); // end of jQuery name space
