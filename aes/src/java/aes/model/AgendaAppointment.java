4ef0f11b1609b8a140176aad92ce694260012e72
Leonorico <leonoricoeduardo.borges@estudante.ufjf.br>
2024-06-21 15:47:58 -0300

    Adicionando novas entidades AgendaAppointment e AgendaAvailable

---
+++ aes/src/java/aes/model/AgendaAppointment.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aes.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Leonorico
+ */
+@Entity
+@Table(name = "tb_agenda_appointment")
+@XmlRootElement
+public class AgendaAppointment implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @ManyToOne//(fetch = FetchType.LAZY)
+    private User user;
+    @ManyToOne//(fetch = FetchType.LAZY)
+    private User consultant;
+    @Column(name = "appointment_date")
+    private LocalDateTime appointmentDate;
+
+    public long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public User getConsultant() {
+        return consultant;
+    }
+
+    public void setConsultant(User consultant) {
+        this.consultant = consultant;
+    }
+
+    public LocalDateTime getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(LocalDateTime appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+    
+    public void removeAppointment() {
+        this.getUser().getAppointmentsUser().remove(this);
+        this.getConsultant().getAppointmentsConsultant().remove(this);
+        this.setUser(null);
+        this.setConsultant(null);
+    }
+        
+}
---
+++ aes/src/java/aes/model/AgendaAvailable.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aes.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Leonorico
+ */
+@Entity
+@Table(name = "tb_agenda_available")
+@XmlRootElement
+public class AgendaAvailable implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @ManyToOne//(fetch = FetchType.LAZY)
+    private User user;
+    @Column(name = "available_date")
+    private LocalDateTime availableDate;
+
+    public long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getAvailableDate() {
+        return availableDate;
+    }
+
+    public void setAvailableDate(LocalDateTime availableDate) {
+        this.availableDate = availableDate;
+    }
+
+    public void removeAvailable() {
+        this.getUser().getAvailablesUser().remove(this);
+        this.setUser(null);
+    }
+    
+    
+}
