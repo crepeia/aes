858da106ef395cfb0bf1f2f1d83b62a4d09c9d34
LuanBarbs <luan.barbosa@estudante.ufjf.br>
2026-01-08 16:49:37 -0300

    adicionando segurança em notification

--- aes/src/java/aes/service/MobileOptionsFacadeREST.java
+++ aes/src/java/aes/service/MobileOptionsFacadeREST.java
@@ -9,12 +9,9 @@ import aes.model.MobileOptions;
 import aes.model.User;
 import aes.persistence.MobileOptionsDAO;
 import aes.persistence.UserDAO;
-import aes.utility.ExpoNotification;
 import aes.utility.Secured;
 import aes.utility.SecurityContextHelper;
 import java.sql.SQLException;
-import java.time.OffsetDateTime;
-import java.time.OffsetTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -26,19 +23,15 @@ import javax.ejb.TransactionManagementType;
 import javax.inject.Inject;
 import javax.naming.NamingException;
 import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.SecurityContext;
 
 /**
  *
